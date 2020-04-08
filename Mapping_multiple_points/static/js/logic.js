8f72643bda44ab2e47a2e615aab46ec8b66fa3a3
Peggy Khiev <pegkhiev@Peggys-MBP.attlocal.net>
2020-04-07 21:27:53 -0700

    Mapping Multiple Points files

--- Mapping_multiple_points/static/js/logic.js
+++ Mapping_multiple_points/static/js/logic.js
@@ -1,8 +1,10 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 console.log('logic loading')
-console.log(API_KEY)
 
+
+let cityData = cities;
+  
 
 // Create the map object with a center (lat and long) and zoom level.
 let map  = L.map('mapid').setView([40.7, -94.5], 4);
@@ -18,16 +20,29 @@ let map  = L.map('mapid').setView([40.7, -94.5], 4);
 
 //  Add a marker to the map for Los Angeles, California.
 // let marker = 
-L.circle([34.0522, -118.2437], 
-        {radius:300,
-        color: "black",
-        fillColor: '#FFFF00',
-        fillOpacity: 0.5
-    }).addTo(map);
-
+// L.circle([34.0522, -118.2437], 
+//         {radius:300,
+//         color: "black",
+//         fillColor: '#FFFF00',
+//         fillOpacity: 0.5
+//     }).addTo(map);
+    
+cityData.forEach(function(city) {
+	console.log(city)
+	L.circleMarker(city.location, {
+    radius:(city.population/200000),
+    color: "orange",
+    fillColor: "#f03",
+    fillOpacity: 0.5,
+    weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + 
+        city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 // / We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
